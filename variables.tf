1620f575d1e5885968a83057a7d0b9c6e43b4b44
Maheshrjl <62394512+Maheshrjl@users.noreply.github.com>
2021-06-06 12:05:59 +0530

    updated variable descriptions

--- variables.tf
+++ variables.tf
@@ -1,10 +1,12 @@
+# All variables defined here have to be given a value in the terraform.tfvarsfile
+
 variable "region" {
     default = "us-east-1" #Default value will be used unless it is updated in tfvars file
 }
-variable "vpc_cidr_block" {}
-variable "subnet_cidr_block" {}
-variable "avail_zone" {}
-variable "env_prefix" {}
+variable "vpc_cidr_block" {} #"10.0.0.0/16"
+variable "subnet_cidr_block" {} #"10.0.10.0/24"
+variable "avail_zone" {} #"ap-south-1a"
+variable "env_prefix" {} #"development"
 variable "my_ip" {} #Write your ip in CIDR notation Eg: YourIpAddress/32
-variable "machine_ami" {}
-variable "usr_public_key" {}
+variable "machine_ami" {} #"ami-010aff33ed5991201"
+variable "usr_public_key" {}# Your public key is located here: ~/.ssh/id_rsa.pub If not create the key using ssh-keygen
