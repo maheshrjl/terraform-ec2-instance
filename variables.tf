08c0e0a71a1292637bb5292583d19d77b2d5ce77
unknown <62394512+Maheshrjl@users.noreply.github.com>
2021-07-07 13:55:59 +0530

    update variables

--- variables.tf
+++ variables.tf
@@ -1,24 +1,32 @@
 # All variables defined here have to be given a value using the terraform.tfvarsfile
 variable "region" {
-    default = "ap-south-1"
+    default = "ap-south-1" #This is the region
 }
+
 variable "instance_count" {
-    default = 2
+    default = 2 #This is the number of instances
 }
+
 variable "vpc_cidr_block" {
     default = "10.0.0.0/16"
 }
+
 variable "subnet_cidr_block" {
     default = "10.0.10.0/24"
 }
+
 variable "avail_zone" {
-    default = "ap-south-1a"
+    default = "ap-south-1a" #This is the AZ in the region, all resources are created in this AZ
 }
+
 variable "env_prefix" {
-    default = "development"
+    default = "development" #This is the name of the resources in format development0, development1
 }
+
 variable "usr_public_key" {
-    default = "~/.ssh/id_rsa.pub"
-}# Give a path to your public key: ~/.ssh/id_rsa.pub If not create the key using ssh-keygen
-variable "my_ip" {} #Write your ip in CIDR notation Eg: YourIpAddress/32
+    default = "~/.ssh/id_rsa.pub"# Give a path to your public key: ~/.ssh/id_rsa.pub If not create the key using ssh-keygen
+}
+variable "my_ip" {} #Write your ip in CIDR notation Eg: YourIpAddress/32. This IP is used to create security groups
 variable "machine_ami" {} #"ami-010aff33ed5991201"
+
+#It is recommended to create a separate terraform.tfvars file & define all variables
